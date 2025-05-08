30d9f7932acb574945a985860a316bbc636a6eab
Majok-Bol <bolmajok001@gmail.com>
2025-05-08 14:31:33 +0300

    added icons in appbar

--- lib/features/home/bloc/home_event.dart
+++ lib/features/home/bloc/home_event.dart
@@ -1,4 +1,17 @@
 part of 'home_bloc.dart';
 
 @immutable
-sealed class HomeEvent {}
+abstract class HomeEvent {}
+
+//first event
+class HomeProductWishlistButtonClickedEvent extends HomeEvent {}
+
+//second event
+class HomeProductCartButtonClickedEvent extends HomeEvent {}
+
+//third event
+//go to new page
+class HomeWishlistButtonNavigateEvent extends HomeEvent {}
+
+//fourth event
+class HomeCartButtonNavigateEvent extends HomeEvent {}
--- lib/features/home/bloc/home_state.dart
+++ lib/features/home/bloc/home_state.dart
@@ -1,6 +1,23 @@
 part of 'home_bloc.dart';
 
 @immutable
-sealed class HomeState {}
+abstract class HomeState {}
 
-final class HomeInitial extends HomeState {}
+abstract class HomeActionState extends HomeState {}
+
+class HomeInitial extends HomeState {}
+
+//loading icon
+class HomeLoadingState extends HomeState {}
+
+//display page after loading
+class HomeLoadedSuccessState extends HomeState {}
+
+//show error if not loaded
+class HomeErrorState extends HomeState {}
+
+//navigate to wishlist page
+class HomeNavigateToWishlistPageActionState extends HomeActionState {}
+
+//navigate to cart page
+class HomeNavigateToCartPageActionState extends HomeActionState {}
