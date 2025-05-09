c039e897e918643edf88e1cc8c68e6767d17472f
Majok-Bol <bolmajok001@gmail.com>
2025-05-09 15:02:03 +0300

    resized container  decoration box

--- lib/features/home/ui/product_tile_widget.dart
+++ lib/features/home/ui/product_tile_widget.dart
@@ -1,40 +1,42 @@
 //handle product tile
 import 'package:cart_project/features/home/models/home_product_data_model.dart';
 import 'package:flutter/cupertino.dart';
-
+import'package:flutter/material.dart';
 class ProductTileWidget extends StatelessWidget{
   final ProductDataModel productDataModel;
   const ProductTileWidget({super.key, required this.productDataModel});
   @override
   Widget build(BuildContext context){
     return Container(
-      padding: EdgeInsets.all(12),
-      margin:EdgeInsets.all(12),
+      width: 300,
+      height: 400,
+      padding: EdgeInsets.all(4),
+      margin:EdgeInsets.all(4),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black),
+        borderRadius: BorderRadius.circular(12),
+      ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
 
         children: [
-          // Container(
-          //   padding: EdgeInsets.only(top:1,bottom: 1),
-          //   child: ClipRect(
-          //   child: Image.network(
-          //     width: 400,
-          //       height: 400,
-          //       productDataModel.imageUrl),
-          //
-          // ),),
           Container(
-            height: 200,
-            width: double.maxFinite,
-            padding: EdgeInsets.all(10),
+            height:320,
+            width: double.infinity,
+            padding: EdgeInsets.only(top:2),
             decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
               image: DecorationImage(
-                // fit: BoxFit.cover,
+                fit: BoxFit.cover,
                   image:NetworkImage(productDataModel.imageUrl))
             ),
           ),
 
-          Text(productDataModel.name),
+          const SizedBox(height: 2,),
+
+          Text(productDataModel.name,style: TextStyle(fontWeight: FontWeight.bold),),
           Text(productDataModel.description),
+          Text('\$${productDataModel.price.toString()}'),
 
         ],
       ),
